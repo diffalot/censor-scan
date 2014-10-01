e9d6a3dfb477666b54680b2aa2d4df2e53cabc34
Andrew Davis <andrew@lightcorp.net>
2014-09-30 23:30:00 -0500

    modularize phantom-scraper

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
-var phantom = require('phantom');
-var q = require('q');
+var phantomScraper = require('./lib/phantom-scraper')
 
-var scrape = function(options) {
-  var deferred = q.defer();
-  phantom.create(function(ph) {
-    return ph.createPage(function(page) {
-      return page.open(options.uri, function(status) {
-        console.log("SITE OPENED?", status);
+var scanSummary = undefined;
+var scanResults = undefined;
 
-        page.includeJs('//code.jquery.com/jquery-2.1.1.min.js', function() {
-          setTimeout(function() {
-            return page.evaluate(function() {
-
-              var trends = [];
-              $trending = $('.landing-page-hottrends-trends-list-container > div')
-              .each(function(ix, element){
-                trends.push($(element).attr('id'))
-              });
-
-              return {
-                trends: trends
-              };
-            }, function(result) {
-              console.log('RESOLVING', result);
-              deferred.resolve(result);
-              page.render('trends.png')
-              ph.exit();
-            });
-          }, 5000);
-
-        });
-      });
-    });
+var scrapeInjection = function() {
+  var trends = [];
+  $trending = jQuery('.landing-page-hottrends-trends-list-container > div')
+  .each(function(ix, element){
+    trends.push(jQuery(element).attr('id'))
   });
-  return deferred.promise;
+  return {
+    trends: trends
+  };
 };
 
-
-var result = scrape({
-  uri: "https://www.google.com/trends/"
+phantomScraper({
+  uri: "https://www.google.com/trends/",
+  scriptInclude: "//code.jquery.com/jquery-2.1.1.min.js",
+  scriptEvaluate: scrapeInjection,
+  evaluationTimeout: 5000,
+  noConflict: function() { $.noConflict(); }
 })
 .then(function(result){
-  console.log('RESOLVED', result);
+  console.log('SCRAPE RESULTS', result);
+  scanSummary = result;
   return result;
 });
+
+module.exports = {
+  scanResults: scanResults,
+  scanSummary: scanSummary
+};
